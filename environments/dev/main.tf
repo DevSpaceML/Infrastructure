8a7d5497d894af89a3c6daf1cd1743c304c06b94
Tech Lead <devspaceml@gmail.com>
2025-09-06 10:49:45 +0200

    update formatting of dev repo [skip ci]

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -13,7 +13,7 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 provider "helm" {
@@ -22,10 +22,10 @@ provider "helm" {
   }
 }
 
-module dev_vpc {
-    source   = "../../modules/network/vpc"
-    cidr     = var.dev_cidr
-    vpcname  = var.vpcname
+module "dev_vpc" {
+  source  = "../../modules/network/vpc"
+  cidr    = var.dev_cidr
+  vpcname = var.vpcname
 }
 
 module "iam" {
@@ -33,21 +33,21 @@ module "iam" {
 }
 
 module "dev_cluster" {
-  depends_on         = [ module.dev_vpc ]
-  source             = "../../modules/compute/eks/cluster"
-  kubeadmin-arn      = var.kubeadmin-arn
-  devops_user        = var.devops_user 
-  clustername        = var.clustername
-  vpcId              = module.dev_vpc.eks_vpc_id
-  public_subnet_ids  = module.dev_vpc.public_subnets
-  public_cidr        = module.dev_vpc.public_cidr
-  cluster_role_arn   = module.iam.cluster_role_arn  
-  access_entries     = var.access_entries
+  depends_on        = [module.dev_vpc]
+  source            = "../../modules/compute/eks/cluster"
+  kubeadmin-arn     = var.kubeadmin-arn
+  devops_user       = var.devops_user
+  clustername       = var.clustername
+  vpcId             = module.dev_vpc.eks_vpc_id
+  public_subnet_ids = module.dev_vpc.public_subnets
+  public_cidr       = module.dev_vpc.public_cidr
+  cluster_role_arn  = module.iam.cluster_role_arn
+  access_entries    = var.access_entries
 
- }
+}
 
 module "dev_nodes" {
-  depends_on         = [ module.dev_cluster, module.iam ]
+  depends_on         = [module.dev_cluster, module.iam]
   source             = "../../modules/compute/eks/nodegroups"
   node_group_mgr_arn = module.iam.node_manager_role_arn.arn
   nodegroupname      = var.nodegroupname
@@ -61,50 +61,50 @@ data "aws_eks_cluster_auth" "devcluster" {
 
 # Allow provider to be passed from caller
 provider "kubernetes" {
-  alias = "this"
+  alias                  = "this"
   host                   = module.dev_cluster.cluster_endpoint
   cluster_ca_certificate = module.dev_cluster.cluster_certificate
   token                  = data.aws_eks_cluster_auth.devcluster.token
 }
 
 module "auth" {
-  depends_on = [ module.dev_cluster, module.dev_nodes ]
+  depends_on = [module.dev_cluster, module.dev_nodes]
 
-  source          = "../../modules/security/auth"
-  eksclustername  = module.dev_cluster.cluster_name
-  noderolearn     = module.iam.node_manager_role_arn.arn
-  hosturl         = module.dev_cluster.cluster_endpoint
+  source         = "../../modules/security/auth"
+  eksclustername = module.dev_cluster.cluster_name
+  noderolearn    = module.iam.node_manager_role_arn.arn
+  hosturl        = module.dev_cluster.cluster_endpoint
 
   providers = {
     kubernetes = kubernetes.this
-  }  
+  }
 }
 
 module "certs" {
-  depends_on = [ module.dev_cluster, module.auth, module.dev_nodes ]
+  depends_on = [module.dev_cluster, module.auth, module.dev_nodes]
 
-  source = "../../modules/security/certs"
-  security-groups     = module.dev_vpc.eks_security_group_id
-  private_cidr_block  = module.dev_vpc.private_cidr
-  public_cidr_block   = module.dev_vpc.public_cidr
-  eks_subnets         = module.dev_vpc.public_subnets
-  securitygroupId     = module.dev_vpc.eks_security_group_id
-  cluster_vpc_id      = module.dev_vpc.eks_vpc_id
-  eksclustername      = module.dev_cluster.cluster_name
+  source             = "../../modules/security/certs"
+  security-groups    = module.dev_vpc.eks_security_group_id
+  private_cidr_block = module.dev_vpc.private_cidr
+  public_cidr_block  = module.dev_vpc.public_cidr
+  eks_subnets        = module.dev_vpc.public_subnets
+  securitygroupId    = module.dev_vpc.eks_security_group_id
+  cluster_vpc_id     = module.dev_vpc.eks_vpc_id
+  eksclustername     = module.dev_cluster.cluster_name
 }
 
 module "route53" {
-  depends_on = [ module.certs ]
-  source = "../../modules/dns/route53"
+  depends_on         = [module.certs]
+  source             = "../../modules/dns/route53"
   private_cidr_block = module.dev_vpc.private_cidr
   public_cidr_block  = module.dev_vpc.public_cidr
   securitygroupId    = module.dev_vpc.eks_security_group_id
   ekscertificate     = module.certs.ekscertificate
-  certarn            = module.certs.cert_arn 
+  certarn            = module.certs.cert_arn
 }
 
 module "rds_appdata" {
-  depends_on = [ module.dev_vpc ]
+  depends_on         = [module.dev_vpc]
   source             = "../../modules/database/rds"
   private_subnet_Ids = module.dev_vpc.private_subnets
 }
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,21 +1,21 @@
 variable "clustername" {
   description = "name of the dev cluster"
-  type = string
+  type        = string
 }
 
 variable "k8s_version" {
-	description = "Version to be used with the cluster"
-	type = string
+  description = "Version to be used with the cluster"
+  type        = string
 }
 
 variable "region" {
-	description = "aws region"
-	type = string
+  description = "aws region"
+  type        = string
 }
 
 variable "instancetype" {
-	description = "Default Instance Type"
-	type = string
+  description = "Default Instance Type"
+  type        = string
 }
 
 variable "vpcname" {
@@ -24,29 +24,29 @@ variable "vpcname" {
 }
 
 variable "dev_cidr" {
-	description = "Default cidr block for VPC cluster"
-	type = string
+  description = "Default cidr block for VPC cluster"
+  type        = string
 }
 
 variable "nodegroupname" {
-	description = "Name of the nodegroup"
-	type = string
-  default = ""
+  description = "Name of the nodegroup"
+  type        = string
+  default     = ""
 }
 
 variable "desired_node_count" {
-	description = "number of nodes requested by user"
-	type = number
+  description = "number of nodes requested by user"
+  type        = number
 }
 
 variable "max_node_count" {
-	description = "max nodes allowed"
-	type = number
+  description = "max nodes allowed"
+  type        = number
 }
 
 variable "min_node_count" {
-	description = "minimum nodes for any deployment"
-	type = number
+  description = "minimum nodes for any deployment"
+  type        = number
 }
 
 variable "kubeadmin-arn" {
@@ -62,16 +62,16 @@ variable "devops_user" {
 variable "private_subnet_Ids" {
   description = "list of private subnet ids"
   type        = list(string)
-  default = []
+  default     = []
 }
 
 variable "access_entries" {
   description = "Map of access entries to create"
-  type        = map(object({
-	principal_arn = string
-	kubernetes_groups = optional(list(string), [])
-	type              = optional(string, "STANDARD")
-	user_name         = optional(string)
-	tags = optional(map(string), {})
+  type = map(object({
+    principal_arn     = string
+    kubernetes_groups = optional(list(string), [])
+    type              = optional(string, "STANDARD")
+    user_name         = optional(string)
+    tags              = optional(map(string), {})
   }))
 }
