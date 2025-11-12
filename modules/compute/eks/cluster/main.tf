3ebc8b0dbd29691c9c757059ccaa2f244430062b
Tech Lead <devspaceml@gmail.com>
2025-11-12 11:58:48 +0200

    update cluster vpc config [skip ci]

--- modules/compute/eks/cluster/main.tf
+++ modules/compute/eks/cluster/main.tf
@@ -18,7 +18,7 @@ resource "aws_eks_cluster" "this" {
 	role_arn = var.cluster_role_arn
 	
 	vpc_config {
-		subnet_ids              = [var.public_subnet_ids, var.private_subnet_ids]
+		subnet_ids              = [var.private_subnet_ids]
 		endpoint_public_access  = var.endpoint_public_access
 		endpoint_private_access = var.endpoint_private_access
     security_group_ids      = var.additional_security_group_ids
