80f3317300e4d1a4193b822b7383a2b48f1e13df
Tech Lead <devspaceml@gmail.com>
2025-11-18 12:28:55 +0200

    additional security group rules for nodes [skip ci]

--- modules/network/eks-security-groups/main.tf
+++ modules/network/eks-security-groups/main.tf
@@ -21,9 +21,9 @@ resource "aws_security_group" "nodegroup-sg" {
 }
 
 
-# rule for note-to-cluster communication
+# allow communication between cluster and nodegroup
 resource "aws_security_group_rule" "cluster_to_nodegroup" {
-    type                     = "egress"
+    type                     = "ingress"
     from_port                = 443                
     to_port                  = 443
     protocol                 = "tcp"
@@ -31,25 +31,56 @@ resource "aws_security_group_rule" "cluster_to_nodegroup" {
     source_security_group_id = data.aws_security_group.cluster-sg.id
 }
 
-#rule for cluster-to-node communication
+# allow nodes to communicate with cluster
 resource "aws_security_group_rule" "nodegroup_to_cluster" {
-    type                     = "ingress"
+    type                     = "egress"
     from_port                = 443                        
     to_port                  = 443
     protocol                 = "tcp"
-    cidr_blocks              = var.nodegroup_cidr_blocks
-    security_group_id        = data.aws_security_group.cluster-sg.id
-    source_security_group_id = aws_security_group.nodegroup-sg.id
+    security_group_id        = aws_security_group.nodegroup-sg.id 
+    source_security_group_id = data.aws_security_group.cluster-sg.id
 }
 
+
+# allow cluster to communicate with nodes on kubelet port
 resource "aws_security_group_rule" "node_ingress_cluster_kubelet" {
    type = "ingress"
    from_port = 10250
    to_port = 10250
    protocol = "tcp"
-   cidr_blocks = var.nodegroup_cidr_blocks
    security_group_id = aws_security_group.nodegroup-sg.id
    source_security_group_id = data.aws_security_group.cluster-sg.id
 } 
 
+# allow nodes to communicate with each other
+resource "aws_security_group_rule" "node_to_node" {
+    type              = "ingress"
+    from_port         = 0
+    to_port           = 65535
+    protocol          = "tcp"
+    security_group_id = aws_security_group.nodegroup-sg.id
+    source_security_group_id = aws_security_group.nodegroup-sg.id
+    description = "Allow nodes to communicate with each other"
+}
 
+# cluster to nodes - extended range 
+resource "aws_security_group_rule" "cluster_to_node_extended" {
+    type                     = "ingress"
+    from_port                = 1025                        
+    to_port                  = 65535
+    protocol                 = "tcp"
+    security_group_id        = aws_security_group.nodegroup-sg.id 
+    source_security_group_id = data.aws_security_group.cluster-sg.id
+    description = "Allow cluster to communicate with nodes ephemeral ports"
+}
+
+# allow nodes to reach internet for updates etc
+resource "aws_security_group_rule" "node_egress_internet" {
+    type              = "egress"
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    security_group_id = aws_security_group.nodegroup-sg.id
+    cidr_blocks      = ["0.0.0.0/0"]
+    description = "Allow nodes to reach internet"
+}
