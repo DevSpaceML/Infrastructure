280d148d71f9bbdbd5a2bcb35c1534a0a3f8979b
Tech Lead <devspaceml@gmail.com>
2025-12-08 16:51:28 +0200

    remove dev cidr variables [skip ci]

--- environments/dev/network/variables.tf
+++ environments/dev/network/variables.tf
@@ -13,13 +13,3 @@ variable "vpcname" {
   description = "name of vpc used by dev cluster"
   type        = string
 }
-
-variable "private_cidr_blocks" {
-  description = "Private Cidr blocks"
-  type = string
-}
-
-variable "public_cidr_blocks" {
-  description = "Public cidr blocks"
-  type = string
-}
