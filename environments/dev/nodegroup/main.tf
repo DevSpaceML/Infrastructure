d314f28a0bc7dfc7589346e3e1fd8624957f3f02
Tech Lead <devspaceml@gmail.com>
2025-12-08 17:36:46 +0200

    correction to input cluster output names [skip ci]

--- environments/dev/nodegroup/main.tf
+++ environments/dev/nodegroup/main.tf
@@ -1,8 +1,8 @@
 module "dev_nodes" {
   source                       = "../../../modules/compute/eks/nodegroups"
   node_group_mgr_arn           = data.terraform_remote_state.dev_iam.outputs.node-mgr-arn
-  nodegroupname                = "${data.terraform_remote_state.dev_cluster.outputs.clustername}-nodegroup"
-  eksclustername               = data.terraform_remote_state.dev_cluster.outputs.clustername
+  nodegroupname                = "${data.terraform_remote_state.dev_cluster.outputs.cluster_name}-nodegroup"
+  eksclustername               = data.terraform_remote_state.dev_cluster.outputs.cluster_name
   k8s_version                  = var.k8s_version
   instancetype                 = var.instancetype
   nodegroup_pvt_subnet_id_list = data.terraform_remote_state.dev_network.outputs.nodegroup_subnet_id_list
