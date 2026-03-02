4511607c1e2e830f6715c4c9425a579b61708a81
Tech Lead <devspaceml@gmail.com>
2026-03-02 12:10:34 -0500

    update network variables

--- environments/dev/network/variables.tf
+++ environments/dev/network/variables.tf
@@ -7,16 +7,19 @@ variable "dev_cidr" {
 variable "region" {
   description = "aws region"
   type        = string
+  default = ""
 }
 
 variable "vpcname" {
   description = "name of vpc used by dev cluster"
   type        = string
+  default = ""
 }
 
 variable "createvpc" {
   description = "creates dedicated vpc if true"
   type = bool
+  default = false
 }
 
 variable "rds_private_subnet_cidr_blocks" {
