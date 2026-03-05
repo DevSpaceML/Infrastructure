e9cd67b34585e72f29835c0ed7423e00e34544bf
Tech Lead <devspaceml@gmail.com>
2026-03-05 10:24:17 -0500

    update data source filter

--- modules/network/vpc/main.tf
+++ modules/network/vpc/main.tf
@@ -31,8 +31,8 @@ data "aws_vpc" "clustervpcdata" {
 data "aws_vpc" "existing_vpc" {
 	count = var.createvpc ? 0 : 1
 	filter {
-    name   = "vpc-id"
-    values = [var.existing_vpc_id]
+    name   = "tag:vpcname"
+    values = [var.vpcname]
   }
 }
 
