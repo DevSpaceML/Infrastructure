5b4038275c05422c0287a354cbee0ee64f3f4803
Tech Lead <devspaceml@gmail.com>
2026-03-16 11:15:21 -0400

    update workflow and variables

--- environments/dev/cluster/variables.tf
+++ environments/dev/cluster/variables.tf
@@ -2,3 +2,9 @@ variable "clustername" {
   description = "name of the dev cluster"
   type        = string
 }
+
+
+variable "cluster_subnet_id_list" {
+  description = "list of subnet ids for the cluster"
+  type        = list(string)
+}
