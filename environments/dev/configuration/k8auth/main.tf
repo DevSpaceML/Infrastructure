2b72d18970227baf9493f8e6cf2c6d12e260e130
Tech Lead <devspaceml@gmail.com>
2026-03-31 13:25:56 -0400

    update iam outputs

--- environments/dev/configuration/k8auth/main.tf
+++ environments/dev/configuration/k8auth/main.tf
@@ -1,9 +1,9 @@
 module "Dev_k8_auth" {
   source = "../../../../modules/security/auth"
   clustername      = data.terraform_remote_state.dev_cluster.outputs.cluster_name
-  noderolearn      = data.terraform_remote_state.dev_iam.outputs.node-manager-role
+  noderolearn      = data.terraform_remote_state.dev_iam.outputs.node_manager_role_arn
   clusteradminrole = data.terraform_remote_state.dev_iam.outputs.cluster-role-arn
-  DevOpsAdminSre   = data.terraform_remote_state.dev_iam.outputs.DevOps-SRE-Admin
-  techlead         = data.terraform_remote_state.dev_iam.outputs.techlead_developer
+  DevOpsAdminSre   = data.terraform_remote_state.dev_iam.outputs.DevOpsAdminSre-arn
+  techlead         = data.terraform_remote_state.dev_iam.outputs.techlead_developer_arn
   hosturl          = data.terraform_remote_state.dev_cluster.outputs.cluster_endpoint 
-} 
+}
