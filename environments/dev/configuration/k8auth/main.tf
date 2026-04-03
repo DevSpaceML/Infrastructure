40b04af4206e845033ecf8d742c2fa3cb7f8a4cd
Tech Lead <devspaceml@gmail.com>
2026-04-03 12:34:40 -0400

    update k8 role versions

--- environments/dev/configuration/k8auth/main.tf
+++ environments/dev/configuration/k8auth/main.tf
@@ -1,9 +1,9 @@
 module "Dev_k8_auth" {
   source = "../../../../modules/security/auth"
   clustername      = data.terraform_remote_state.dev_cluster.outputs.cluster_name
-  noderolearn      = data.terraform_remote_state.dev_iam.outputs.node_manager_role_arn
+  noderolearn      = data.terraform_remote_state.dev_iam.outputs.node-mgr-arn
   clusteradminrole = data.terraform_remote_state.dev_iam.outputs.cluster-role-arn
   DevOpsAdminSre   = data.terraform_remote_state.dev_iam.outputs.DevOpsAdminSre-arn
-  techlead         = data.terraform_remote_state.dev_iam.outputs.techlead_developer_arn
+  techlead         = data.terraform_remote_state.dev_iam.outputs.techlead-arn
   hosturl          = data.terraform_remote_state.dev_cluster.outputs.cluster_endpoint 
 }
