443951abf311313453399995387f89750ba36d52
Tech Lead <devspaceml@gmail.com>
2026-04-05 13:39:29 -0400

    outputs update

--- environments/dev/cluster/outputs.tf
+++ environments/dev/cluster/outputs.tf
@@ -5,11 +5,6 @@ output "cluster_endpoint" {
 output "cluster_cert" {
   value = module.dev_cluster.cluster_certificate
 }
-
 output "cluster_name" {
   value = module.dev_cluster.cluster_name
-}
-
-output "cluster_token" {
-  value = module.dev_cluster.cluster_endpoint
-}
+} 
