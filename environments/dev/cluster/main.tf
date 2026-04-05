5c500306507d831acbc2f5dc9d161befa330c9c2
Tech Lead <devspaceml@gmail.com>
2026-04-05 13:56:36 -0400

    update output in provider

--- environments/dev/cluster/main.tf
+++ environments/dev/cluster/main.tf
@@ -23,7 +23,7 @@ provider "aws" {
 
 provider "kubernetes" {
    host = module.dev_cluster.cluster_endpoint
-   cluster_ca_certificate = module.dev_cluster.cluster_cert
+   cluster_ca_certificate = module.dev_cluster.cluster_certificate
    
    exec {
 	 api_version = "client.authentication.k8s.io/v1beta1"
