7e2d3847776b55f141bdf8974bf2b86ed7acdce8
Tech Lead <devspaceml@gmail.com>
2026-04-06 15:51:14 -0400

    update k8auth provider

--- environments/dev/configuration/k8auth/main.tf
+++ environments/dev/configuration/k8auth/main.tf
@@ -1,16 +1,11 @@
+data "aws_eks_cluster_auth" "dev_cluster" {
+  name = data.terraform_remote_state.dev_cluster.outputs.cluster_name
+}
+
 provider "kubernetes" {
-   host = module.dev_cluster.cluster_endpoint
-   cluster_ca_certificate = module.dev_cluster.cluster_certificate
-   
-   exec {
-	 api_version = "client.authentication.k8s.io/v1beta1"
-	 command = "aws"
-	 args = [
-      "eks", "get-token",
-      "--cluster-name", module.dev_cluster.cluster_name,
-      "--region",       var.region
-    ]
-   }
+   host = data.terraform_remote_state.dev_cluster.outputs.cluster_endpoint
+   cluster_ca_certificate = base64decode(data.terraform_remote_state.dev_cluster.outputs.cluster_cert)
+   token                  = data.aws_eks_cluster_auth.dev_cluster.token
 }
 
 module "Dev_k8_auth" {
