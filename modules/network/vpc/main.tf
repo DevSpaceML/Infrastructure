53e0732a4e67cb8080434ac9002e539cb921a0d2
Tech Lead <devspaceml@gmail.com>
2026-04-10 11:36:07 -0400

    updates to deal with node joining cluster issue

--- modules/network/vpc/main.tf
+++ modules/network/vpc/main.tf
@@ -226,7 +226,7 @@ resource "aws_route_table" "nodegroup_private_routetable" {
 resource "aws_route_table_association" "nodegroup_private_route_association" {
 	count          =  length(aws_subnet.public_subnet_eks)
 	subnet_id      =  aws_subnet.nodegroup_private_subnet[count.index].id
-	route_table_id = 	aws_route_table.nodegroup_private_routetable[count.index].id
+	route_table_id =  aws_route_table.nodegroup_private_routetable[count.index].id
 }
 
 
