755fc2e20324765c0a79f207091c941246d94635
Tech Lead <devspaceml@gmail.com>
2026-04-15 11:59:22 -0400

    remove standalone Dns config from Dev, update cluster outputs and route53 vars

--- modules/dns/route53/main.tf
+++ modules/dns/route53/main.tf
@@ -2,41 +2,34 @@
 #get lb data dynamically [change to use variables for tags]
 
 data "aws_lb" "eks-lb" {    	
-        name = "eks-cluster-lb"
+    name = "eks-cluster-lb"
 } 
 
-data "aws_route53_zone" "approute" {
-	name  = "approute.me"
+resource "aws_route53_zone" "internal" {
+  count = var.environment == "dev" ? 1 : 0	
+  name = "internal.${var.projectname}.dev"
 }
 
-resource "aws_route53_record" "approute-eks" {
-	name    = "approute.me"
+resource "aws_route53_zone" "public" {
+	count = var.environment == "dev" ? 0:1
+	name  = "${var.projectname}.com"
+}
+
+locals {
+	 deployment = var.environment == "dev" ? "internal.${var.projectname}.dev" : "${var.projectname}.com"
+     zone_id = var.environment == "dev" ? one(aws_route53_zone.internal[*].zone_id) : one(aws_route53_zone.public[*].zone_id)
+     zone_name = var.environment == "dev" ? one(aws_route53_zone.internal[*].name) : one(aws_route53_zone.public[*].name)
+   }
+
+resource "aws_route53_record" "app-eks" {
+	name    = local.deployment
 	type    = "A"
-	zone_id = data.aws_route53_zone.approute.zone_id
+	zone_id = local.zone_id
 	
 	alias {
 		name = data.aws_lb.eks-lb.dns_name
 		zone_id = data.aws_lb.eks-lb.zone_id
 		evaluate_target_health = true
 	}
-
 }
 
-# Create Security Groups
-resource "aws_security_group_rule" "eks-tcp-ingress" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  security_group_id = var.securitygroupId
-  cidr_blocks       = var.public_cidr_blocks
-}
-
-resource "aws_security_group_rule" "node-group-egress" {
-	type 			  = "egress"
-	from_port         = 10250
-	to_port   		  = 10250
-	protocol  		  = "tcp"
-	security_group_id = var.securitygroupId
-	cidr_blocks		  = var.private_cidr_blocks	
-}
