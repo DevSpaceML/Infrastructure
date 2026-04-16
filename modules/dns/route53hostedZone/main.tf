6b854d65062baa488848f57237e5395062f2c63e
Tech Lead <devspaceml@gmail.com>
2026-04-16 16:38:20 -0400

    variable updates in workflow, dns projectname update

--- modules/dns/route53hostedZone/main.tf
+++ modules/dns/route53hostedZone/main.tf
@@ -1,17 +1,17 @@
 # Create Hosted Zones for Application
-#get lb data dynamically [change to use variables for tags]
+
 resource "aws_route53_zone" "internal" {
   count = var.environment == "dev" ? 1 : 0	
-  name = "internal.${var.projectname}.dev"
+  name = "internal.${replace(var.projectname, " ", "-")}.dev"
 }
 
 resource "aws_route53_zone" "public" {
 	count = var.environment == "dev" ? 0:1
-	name  = "${var.projectname}.com"
+	name  = "${replace(var.projectname, " ", "-")}.com"
 }
 
 locals {
-	 deployment = var.environment == "dev" ? "internal.${var.projectname}.dev" : "${var.projectname}.com"
+	 deployment = var.environment == "dev" ? "internal.${replace(var.projectname, " ", "-")}.dev" : "${replace(var.projectname, " ", "-")}.com"
      zone_id = var.environment == "dev" ? one(aws_route53_zone.internal[*].zone_id) : one(aws_route53_zone.public[*].zone_id)
      zone_name = var.environment == "dev" ? one(aws_route53_zone.internal[*].name) : one(aws_route53_zone.public[*].name)
 	 zone_arn = var.environment == "dev" ? one(aws_route53_zone.internal[*].arn) : one(aws_route53_zone.public[*].arn)
