5e545e608729607d7de8395a4dfdcfbd1929ee6f
Tech Lead <devspaceml@gmail.com>
2026-04-16 15:19:48 -0400

    refactor dns for ephemeral deployments

--- modules/dns/route53hostedZone/main.tf
+++ modules/dns/route53hostedZone/main.tf
@@ -16,15 +16,3 @@ locals {
      zone_name = var.environment == "dev" ? one(aws_route53_zone.internal[*].name) : one(aws_route53_zone.public[*].name)
    }
 
-resource "aws_route53_record" "app-eks" {
-	name    = local.deployment
-	type    = "A"
-	zone_id = local.zone_id
-	
-	alias {
-		name = data.aws_lb.eks-lb.dns_name
-		zone_id = data.aws_lb.eks-lb.zone_id
-		evaluate_target_health = true
-	}
-}
-
--- modules/dns/route53hostedZone/variables.tf
+++ modules/dns/route53hostedZone/variables.tf
@@ -8,34 +8,3 @@ variable "environment" {
   type = string
 }
 
-variable "securitygroupId" {
-    description = "security groups of cluster vpc"
-	type = string
-}
-
-variable "private_cidr_blocks" {
-	description = "private cidr block"
-	type        =  list(string)
-}
-
-variable "public_cidr_blocks" {
-	description = "public cidr block"
-	type        =  list(string)
-}
-
-variable "certarn" {
-  description = "arn of the eks certificate"
-  type        = string
-}
-
-variable "ekscertificate" {
-  description = "ACM Certificate"
-  type = object({
-    domain_validation_options = list(object({
-      domain_name           = string
-      resource_record_name  = string
-      resource_record_type  = string
-      resource_record_value = string
-    }))
-  })
-}
