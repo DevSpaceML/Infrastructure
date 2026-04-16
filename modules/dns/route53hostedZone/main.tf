7a8081c9fde998019fefb41297985e8c647a5aed
Tech Lead <devspaceml@gmail.com>
2026-04-16 16:42:51 -0400

    change dns domain name to lowercase

--- modules/dns/route53hostedZone/main.tf
+++ modules/dns/route53hostedZone/main.tf
@@ -2,18 +2,17 @@
 
 resource "aws_route53_zone" "internal" {
   count = var.environment == "dev" ? 1 : 0	
-  name = "internal.${replace(var.projectname, " ", "-")}.dev"
+  name = "internal.${lower(replace(var.projectname, " ", "-"))}.dev"
 }
 
 resource "aws_route53_zone" "public" {
 	count = var.environment == "dev" ? 0:1
-	name  = "${replace(var.projectname, " ", "-")}.com"
+	name  = "${lower(replace(var.projectname, " ", "-"))}.com"
 }
 
 locals {
-	 deployment = var.environment == "dev" ? "internal.${replace(var.projectname, " ", "-")}.dev" : "${replace(var.projectname, " ", "-")}.com"
+	 deployment = var.environment == "dev" ? "internal.${lower(replace(var.projectname, " ", "-"))}.dev" : "${lower(replace(var.projectname, " ", "-"))}.com"
      zone_id = var.environment == "dev" ? one(aws_route53_zone.internal[*].zone_id) : one(aws_route53_zone.public[*].zone_id)
      zone_name = var.environment == "dev" ? one(aws_route53_zone.internal[*].name) : one(aws_route53_zone.public[*].name)
 	 zone_arn = var.environment == "dev" ? one(aws_route53_zone.internal[*].arn) : one(aws_route53_zone.public[*].arn)
    }
-
