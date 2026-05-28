d3f0654ef403a162927cff5e07ec61f29cc31c71
Tech Lead <devspaceml@gmail.com>
2026-05-28 12:56:59 -0400

    update outputs

--- modules/monitoring/prometheus/outputs.tf
+++ modules/monitoring/prometheus/outputs.tf
@@ -1,15 +1,25 @@
 output "amp_url" {
-  value = aws_prometheus_workspace.monitor.workspace_url
+  value = aws_prometheus_workspace.this.workspace_url
 }
 
 output "amp_arn" {
-  value = aws_prometheus_workspace.monitor.arn
+  value = aws_prometheus_workspace.this.arn
 }
 
-output "monitor_namespace" {
-  value = aws_prometheus_workspace.monitor.alias
+output "amp_namespace" {
+  value = aws_prometheus_workspace.this.alias
+}
+
+output "amp_workspace_id" {
+  value = aws_prometheus_workspace.this.id
+}
+
+output "irsa_role_arn" {
+  value = module.irsa_prometheus.iam_role_arn
 }
 
 output "prometheus_svc_acc" {
   value = kubernetes_svc_account_v1.prometheus_agent.metadata[0].name
 }
+
+
