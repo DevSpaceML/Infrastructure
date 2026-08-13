a3befb9d94d2a468ae2b0b7a0d7f1d36fb806ffa
Tech Lead <devspaceml@gmail.com>
2026-08-13 10:08:21 -0400

    remote unnecessary data source from network deployment [skip ci]

--- environments/dev/network/main/data.tf
+++ environments/dev/network/main/data.tf
@@ -1,8 +1,1 @@
-data "terraform_remote_state" "iam" {
-    backend = "s3"
-    config = {
-        bucket = "dev-tf-state-488347380548"
-        key    = "dev/iam/main/terraform.tfstate"
-        region = "us-east-1"
-    }
-}
+
