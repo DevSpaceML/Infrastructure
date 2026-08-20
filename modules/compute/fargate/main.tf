f73c4330307dbf3cde18dca8dfc47731a922fbb8
Tech Lead <devspaceml@gmail.com>
2026-08-20 12:44:41 -0400

    update fargate image [skip ci]

--- modules/compute/fargate/main.tf
+++ modules/compute/fargate/main.tf
@@ -155,7 +155,7 @@ resource "aws_ecs_task_definition" "slfsvc-app" {
   container_definitions = jsonencode([
     {
       name      = "slfsvc-app"
-      image     = "${data.aws_ecr_repository.selfservice.repository_url}:20260814_1017"
+      image     = "${data.aws_ecr_repository.selfservice.repository_url}:20260814_1555"
       essential = true
       portMappings = [{ containerPort = 80, protocol = "tcp" }]
       logConfiguration = {
