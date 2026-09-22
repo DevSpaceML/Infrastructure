69df05c78b536d23c572c58e0722f01f946e86bb
Tech Lead <devspaceml@gmail.com>
2026-09-22 12:49:03 -0400

    changes for shared-alb [skip ci]

--- modules/alb/dev/eks-alb/main.tf
+++ modules/alb/dev/eks-alb/main.tf
@@ -2,8 +2,13 @@ resource "aws_lb" "k8_shared" {
   name               = "k8_shared_alb"
   internal           = false
   load_balancer_type = "application"
+  security_groups    = [var.alb_securitygroup_id]
   subnets            = var.public_subnet_ids
-  security_groups    = var.alb_securitygroup_id
+
+  tags = {
+    Name        = "k8-shared-alb"
+    Environment = "Dev"
+  }
 }
 
 
