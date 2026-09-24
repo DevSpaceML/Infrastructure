72a6142cf4b3cb142efb874b1b8a86e52a773fa8
Tech Lead <devspaceml@gmail.com>
2026-09-24 12:03:28 -0400

    update to access_entries [skip ci]

--- modules/compute/eks/cluster/outputs.tf
+++ modules/compute/eks/cluster/outputs.tf
@@ -7,10 +7,6 @@ output "cluster_name" {
   value = aws_eks_cluster.this.name
 }
 
-output "name" {
-  value = aws_eks_cluster.this.namespace
-}
-
 output "cluster_token" {
   value = aws_eks_cluster.this.identity[0].oidc[0].issuer
   description = "Authentication token for the EKS cluster"
