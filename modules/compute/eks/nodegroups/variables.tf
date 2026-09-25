54ab9d830f2c706397cf1ca9ba13d65c691a9a3e
Tech Lead <devspaceml@gmail.com>
2026-09-25 12:02:01 -0400

    Change default instancy type [skip ci]

--- modules/compute/eks/nodegroups/variables.tf
+++ modules/compute/eks/nodegroups/variables.tf
@@ -13,7 +13,7 @@ variable "nodegroupname" {
 variable "instancetype" {
 	description = "Default Instance Type"
 	type = string
-	default = "t4g.small"
+	default = "t3.small"
 }
 
 variable "desired_node_count" {
